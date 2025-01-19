b5280e7620ba34eadc22e7ad7a22ec8aeb4b34d0
Peter Hollaubek <peter.hollaubek@gmail.com>
2025-01-18 19:45:26 -0500

    convert character to class and add update all

--- wow-io.js
+++ wow-io.js
@@ -1,5 +1,62 @@
 let myCharacters = [];
 
+class Character {
+    constructor(realm, name) {
+        this.realm = realm;
+        this.name = name;
+        this.class = null;
+        this.spec = null;
+        this.role = null;
+        this.ilvl = null;
+        this.io = null;
+        this.best_runs = {};
+        this.updated = null;
+    }
+
+    key() {
+        return `${this.realm}-${this.name}`;
+    }
+
+    async fetchCharacter() {
+        const myRequest = new Request("https://raider.io/api/v1/characters/profile?" + new URLSearchParams({
+            "region": "us",
+            "realm": this.realm,
+            "name": this.name,
+            "fields": "mythic_plus_scores_by_season:current,mythic_plus_best_runs,gear"
+        }));
+
+        const response = await fetch(myRequest);
+        if (response.status == 500) {
+            window.alert("Server error! Please try again later.");
+            return;
+        }
+        const data = await response.json();
+        if (data.statusCode == 400) {
+            window.alert(data.message);
+            return;
+        }
+        this.class = data.class;
+        this.spec = data.active_spec_name;
+        this.role = data.active_spec_role;
+        this.ilvl = Math.round(data.gear.item_level_equipped);
+        this.io = data.mythic_plus_scores_by_season[0].scores.all;
+
+        // get best key runs
+        const runs = {};
+        for (const run of data.mythic_plus_best_runs) {
+            let key_level = run.mythic_level.toString();
+            if (run.num_keystone_upgrades > 0) {
+                key_level += "*".repeat(run.num_keystone_upgrades);
+            }
+            runs[run.short_name] = key_level;
+        }
+        this.best_runs = runs;
+        this.updated = new Date();
+
+        return true;
+  }
+}
+
 loadData();
 
 // add character form
@@ -20,11 +77,13 @@ for (const realm of realms) {
     realmInput.appendChild(option);
 }
 form.appendChild(realmInput);
+// Character name input
 const characterInput = document.createElement("input");
 characterInput.id = "inputCharacter";
 characterInput.type = "text";
 characterInput.placeholder = "Character";
 form.appendChild(characterInput);
+
 const submitButton = document.createElement("button");
 submitButton.type = "submit";
 submitButton.innerText = "Add Character";
@@ -33,52 +92,65 @@ formDiv.appendChild(form);
 
 document.getElementById("addNewCharacterForm").addEventListener("submit", clicked);
 
-
 function clicked(event) {
+    // prevent a reload
     event.preventDefault();
-    console.log("Clicked!");
-    const realm = document.getElementById("inputRealm").value;
-    const characterName = document.getElementById("inputCharacter").value;
-    const characterObj = {
-        realm: realm,
-        character: characterName,
-        spec: "unknown",
-        role: "unknown",
-        ilvl: 0,
-        io: 0,
-        best_runs: {},
-    };
-    myCharacters.push(characterObj);
 
-    saveData();
-    updateCharacter(characterObj);
+    const newCharacterRealm = document.getElementById("inputRealm").value;
+    const newCharacterName = document.getElementById("inputCharacter").value;  
 
-    refreshTable();
+    // check if character already exists
+    for (const character of myCharacters) {
+        if (character.realm == newCharacterRealm && character.name == newCharacterName) {
+            window.alert("Character already exists!");
+            return;
+        }
+    }
+
+    // add new character
+    const newCharacter = new Character(newCharacterRealm, newCharacterName);
+    newCharacter.fetchCharacter().then(response => {
+        if (response) {
+            myCharacters.push(newCharacter);
+            saveData();
+            refreshTable();
+        }
+    });
 }
 
 function saveData() {
-    console.log("Saving data...");
+    // save data to local storage
     localStorage.setItem("ioCharacters", JSON.stringify(myCharacters));
 }
 
 function loadData() {
-    console.log("Loading data...");
+    // reload data from local storage
     const data = localStorage.getItem("ioCharacters");
     if (data) {
-        myCharacters = JSON.parse(data);
+        for (const character of JSON.parse(data)) {
+            const newCharacter = new Character(character.realm, character.name);
+            newCharacter.class = character.class;
+            newCharacter.spec = character.spec;
+            newCharacter.role = character.role;
+            newCharacter.ilvl = character.ilvl;
+            newCharacter.io = character.io;
+            newCharacter.best_runs = character.best_runs;
+            newCharacter.updated = new Date(character.updated);
+            myCharacters.push(newCharacter);
+        }
     }
 
     refreshTable();
 }
 
 function refreshTable() {
-    console.log("Refreshing table...");
+    // refresh table with characters
     const tableBody = document.getElementById("characterTableDiv");
     tableBody.innerHTML = "";
     const myTable = document.createElement("table");
     tableBody.appendChild(myTable);
 
-    const fields = ["#", "Realm", "Character", "Role", "Spec", "iLvl", "IO"];
+    const fields = ["#", "Realm", "Name", "Class", "Spec", "Role", "iLvl", "IO"];
     const dungeons = ["ARAK", "COT", "DAWN", "GB", "MISTS", "NW", "SIEGE", "SV"]
 
     // add header row
@@ -110,6 +182,12 @@ function refreshTable() {
         cell.innerText = dungeon;
         header.appendChild(cell);
     }
+
+    const updated = document.createElement("th");
+    updated.style.width = "200px";
+    updated.style.textAlign = "left";
+    updated.innerText = "Updated";
+    header.appendChild(updated);
 
     let index = 1;
     for (const character of myCharacters.sort((a, b) => b.io - a.io)) {
@@ -149,74 +227,82 @@ function refreshTable() {
             row.appendChild(cell);
         }
 
+        const updated = document.createElement("td");
+        updated.style.width = "200px";
+        updated.innerText = character.updated.toLocaleString();
+        row.appendChild(updated);
+
         // delete buttom for each row
+        const deleteButtonCell = document.createElement("td");
         const deleteButton = document.createElement("button");
         deleteButton.character = character;
         deleteButton.innerText = "Delete";
         deleteButton.addEventListener("click", handleEvent);
-        row.appendChild(deleteButton);
+        deleteButtonCell.appendChild(deleteButton);
+        row.appendChild(deleteButtonCell);
 
+        // update button for each row
+        const updateButtonCell = document.createElement("td");
         const updateButton = document.createElement("button");
         updateButton.character = character;
         updateButton.innerText = "Update";
         updateButton.addEventListener("click", handleEvent);
-        row.appendChild(updateButton);
-        
+        updateButtonCell.appendChild(updateButton);
+        row.appendChild(updateButtonCell);
+
         myTable.appendChild(row);
+
     }
+
+    // add row to table
+    const update_all_row = document.createElement("tr");
+    const update_all_cell = document.createElement("td");
+    update_all_cell.style.textAlign = "center";
+    update_all_cell.colSpan = 19;
+    const update_all_button = document.createElement("button");
+    update_all_button.innerText = "Update All";
+    update_all_button.addEventListener("click", handleEvent);
+    update_all_row.appendChild(update_all_cell);
+    update_all_cell.appendChild(update_all_button);
+    myTable.appendChild(update_all_row);
 }
 
 function handleEvent(event) {
-    console.log("Handing %s event for %s", event.target.innerText, event.target.character.character);
+    // handle events for buttons
     const action = event.target.innerText;
 
+    // update character
     if (action == "Update") {
-        updateCharacter(event.target.character);
+        event.target.character.fetchCharacter().then(response => {
+            if (response) {
+                saveData();
+                refreshTable();
+            }
+        });
     }
+    // delete character
     else if (action == "Delete") {
         deleteCharacter(event.target.character);
     }
+    // update all characters
+    else if (action == "Update All") {
+        for (const character of myCharacters) {
+            character.fetchCharacter().then(response => {
+                if (response) {
+                    saveData();
+                    refreshTable();
+                }
+            });
+        }
+    }   
     else {
         console.error("Unknown action: %s", action);
     }
     
 }
 
-function updateCharacter(character) {
-    // Update character from raider.io
-    console.log(`Updating character: ${character.character}`);
-    const myRequest = new Request("https://raider.io/api/v1/characters/profile?" + new URLSearchParams({
-        "region": "us",
-        "realm": character.realm,
-        "name": character.character,
-        "fields": "mythic_plus_scores_by_season:current,mythic_plus_best_runs,gear"
-    
-    }));
-    fetch(myRequest)
-        .then(response => response.json())
-        .then(data => {
-            character.spec = data.active_spec_name;
-            character.ilvl = Math.round(data.gear.item_level_equipped);
-            character.io = data.mythic_plus_scores_by_season[0].scores.all;
-            character.role = data.active_spec_role;
-            // get best key runs
-            const runs = {};
-            for (const run of data.mythic_plus_best_runs) {
-                let key_level = run.mythic_level.toString();
-                if (run.num_keystone_upgrades > 0) {
-                    key_level += "*".repeat(run.num_keystone_upgrades);
-                }
-                runs[run.short_name] = key_level;
-            }
-            character.best_runs = runs;
-            saveData();
-            refreshTable();
-        });
-}
-
 function deleteCharacter(character) {
     // Delete character from the table 
-    console.log("Delete button clicked!");
     const index = myCharacters.indexOf(character);
     myCharacters.splice(index, 1);
     saveData();
