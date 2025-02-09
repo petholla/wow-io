d72d704e4e0bcd4ccc63c389cb7e732b6e9e515a
Peter Hollaubek <peter.hollaubek@gmail.com>
2025-02-09 16:14:32 -0500

    switch to absolute paths

--- src/wow-io.js
+++ src/wow-io.js
@@ -1,6 +1,6 @@
-import Character from "./character.js";
-import { addNewCharacterForm, addNotesSection } from "./ui.js";
-import { refreshCounter } from "./refresh.js";
+import Character from "/src/character.js";
+import { addNewCharacterForm, addNotesSection } from "/src/ui.js";
+import { refreshCounter } from "/src/refresh.js";
 
 let myCharacters = [];
 
